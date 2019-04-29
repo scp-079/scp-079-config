372ba1d2e38286a87d15ebc24add4782f83b6f1f
Xiaos <xiao@scp-079.org>
2019-04-29 07:59:45 +0000

    0.0.3

---
+++ plugins/functions/channel.py
@@ -0,0 +1,47 @@
+# SCP-079-CONFIG - Manage the settings of each bot
+# Copyright (C) 2019 SCP-079 <https://scp-079.org>
+#
+# This file is part of SCP-079-CONFIG.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+from typing import List
+
+from pyrogram import Client
+
+from .. import glovar
+from .etc import format_data, thread
+from .telegram import send_message
+
+
+# Enable logging
+logger = logging.getLogger(__name__)
+
+
+def share_data(client: Client, sender: str, receivers: List[str], action: str, action_type: str, data=None) -> bool:
+    try:
+        text = format_data(
+            sender=sender,
+            receivers=receivers,
+            action=action,
+            action_type=action_type,
+            data=data
+        )
+        thread(send_message, (client, glovar.exchange_channel_id, text))
+        return True
+    except Exception as e:
+        logger.warning(f"Share data error: {e}", exc_info=True)
+
+    return False
