677ff34da63fe86c4f14606f19e78c987c954b9f
Xiao <xiao@scp-079.org>
2019-08-13 08:58:44 +0000

    - 调整代码格式

--- plugins/functions/timers.py
+++ plugins/functions/timers.py
@@ -36,6 +36,7 @@ def update_status(client: Client) -> bool:
             action_type="status",
             data="awake"
         )
+
         return True
     except Exception as e:
         logger.warning(f"Update status error: {e}")
