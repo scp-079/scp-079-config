878dea7a1f759c3344eaa5853179ed120e61406c
Xiao <xiao@scp-079.org>
2019-10-18 05:27:48 +0000

    - modify lang dict

--- plugins/glovar.py
+++ plugins/glovar.py
@@ -180,7 +180,7 @@ lang: Dict[str, str] = {
     # NOPORN
     "noporn_channel": (zh_cn and "过滤频道") or "Filter Restricted Channel Message",
     # NOSPAM
-    "bio": (zh_cn and "检查用户简介") or "Check Bio",
+    "bio": (zh_cn and "简介封禁") or "Ban by Bio",
     "bot": (zh_cn and "阻止机器人进群") or "Prevent Bot joining",
     "new": (zh_cn and "新入群限制") or "Limit the New Joined User",
     "deleter": (zh_cn and "仅删除") or "Delete Only",
